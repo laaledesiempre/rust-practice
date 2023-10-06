3cb76fb294eb5a4ee6ca6ddde405f1d927bf8746
laaledesiempre <128645176+laaledesiempre@users.noreply.github.com>
2023-10-06 17:33:09 +0000

    continued with tutorial

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,8 +1,10 @@
 use std::io;
+use rand::Rng;
 
 fn main() {
     println!("Guess the number!");
-
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+    println!("{secret_number}");
     println!("Please input ur guess");
 
     let mut guess = String::new();
